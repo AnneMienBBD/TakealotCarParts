1c5599f0e809a95c94c7c3d1775e9b409a99716c
AnneMienBBD <122455277+AnneMienBBD@users.noreply.github.com>
2023-02-21 17:27:27 +0200

    Update getInventory.sql

--- getInventory.sql
+++ getInventory.sql
@@ -1,16 +1,21 @@
 
-SELECT SUM(QuantityAcquired) - SUM(QuantityOrdered) AS 'CarPart Quantity in Inventory',CarPartName, BrandName, VehicleModel, TypeName
-FROM CarPartAcquisition
-JOIN CarPart
-ON CarPartAcquisition.CarPartId = CarPart.CarPartId
-JOIN CarPartOrder
-ON CarPartOrder.CarPartId = CarPart.CarPartId
-JOIN Vehicle
-ON Vehicle.VehicleId = CarPart.VehicleId
-JOIN PartType
-ON PartType.PartTypeId = CarPart.PartTypeId
-JOIN VehicleBrand
-ON VehicleBrand.BrandId = Vehicle.BrandId
-GROUP BY CarPartName, TypeName, BrandName, VehicleModel
+CREATE PROCEDURE [dbo].[uspStockQuantity]
+AS
+BEGIN 
+  SELECT SUM(QuantityAcquired) - SUM(QuantityOrdered) AS 'Quantity in Inventory',CarPartName, BrandName, VehicleModel, TypeName
+  FROM CarPartAcquisition
+  JOIN CarPart
+  ON CarPartAcquisition.CarPartId = CarPart.CarPartId
+  JOIN CarPartOrder
+  ON CarPartOrder.CarPartId = CarPart.CarPartId
+  JOIN Vehicle
+  ON Vehicle.VehicleId = CarPart.VehicleId
+  JOIN PartType
+  ON PartType.PartTypeId = CarPart.PartTypeId
+  JOIN VehicleBrand
+  ON VehicleBrand.BrandId = Vehicle.BrandId
+  GROUP BY CarPartName, TypeName, BrandName, VehicleModel
+  END
+  
 
 
