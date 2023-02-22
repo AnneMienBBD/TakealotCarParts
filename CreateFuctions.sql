2a2b8c0b71e0dcfe4b2853fba77201129900fd30
Sphelele Msweli <sphelele@bbd.co.za>
2023-02-22 22:29:19 +0200

    add user defined functions

---
+++ CreateFuctions.sql
@@ -0,0 +1,39 @@
+CREATE FUNCTION [dbo].[udfTotalOrderCost]
+(
+  @OrderID int
+)
+RETURNS [decimal]
+BEGIN
+	DECLARE @Cost decimal
+	
+	SELECT @Cost = SUM(QuantityOrdered * CostPrice)
+	FROM
+		CarPartOrder LEFT JOIN CarPart
+		ON CarPartOrder.CarPartId = CarPart.CarPartId
+	WHERE
+		OrderId = @OrderID
+	
+	RETURN @Cost
+
+END
+
+GO
+
+CREATE FUNCTION [dbo].[udfGetReceipt]
+(
+  @OrderID int
+)
+RETURNS TABLE
+AS
+RETURN 
+	SELECT
+		CarPartOrder.CarPartId,
+		CarPartName,
+		CostPrice
+	FROM
+		CarPartOrder LEFT JOIN CarPart
+		ON CarPartOrder.CarPartId = CarPart.CarPartId
+	WHERE
+		CarPartOrder.OrderId = @OrderID
+
+GO
