2f8ee98df907877c3c4d56640fe2a4c4437a487a
Tsepo-G <122514307+Tsepo-G@users.noreply.github.com>
2023-02-23 12:50:12 +0200

    Update CarPartAcqusitionsView.sql

--- CarPartAcqusitionsView.sql
+++ CarPartAcqusitionsView.sql
@@ -1,15 +1,32 @@
 USE CarPartTakealotDB;
 GO
 
-CREATE VIEW vCarPartAcquisitions
+CREATE VIEW vCarPartAcquisition
 AS
-SELECT [CarPartAcquisition].AcquisitionId, [Acquisition].ScrapyardId, ScrapyardName, [CarPartAcquisition].CarPartId, [CarPart].CarPartName, QuantityAcquired
+SELECT [Scrapyard].ScrapyardOwner, ScrapyardName, [Scrapyard].PhoneNumber, [Scrapyard].Email, [Suburb].SuburbName, 
+	[Suburb].City, [Scrapyard].StreetAddress, [CarPart].CarPartName, [VehicleBrand].BrandName, [Vehicle].VehicleModel, 
+	[Vehicle].VehicleYear, QuantityAcquired, [Acquisition].AcquisitionDate
+
 FROM CarPartAcquisition RIGHT JOIN Acquisition 
 ON [CarPartAcquisition].AcquisitionId = [Acquisition].AcquisitionId
+
 RIGHT JOIN Scrapyard
 ON [Acquisition].ScrapyardId = [Scrapyard].ScrapyardId
+
 RIGHT JOIN CarPart
 ON [CarPart].CarPartId = [CarPartAcquisition].CarPartId
-GROUP BY [CarPartAcquisition].AcquisitionId, [Acquisition].ScrapyardId, [CarPartAcquisition].CarPartId, QuantityAcquired, ScrapyardName, [CarPart].CarPartName
+
+RIGHT JOIN Vehicle
+ON [CarPart].VehicleId = [Vehicle].VehicleId
+
+RIGHT JOIN VehicleBrand
+ON [Vehicle].BrandId = [VehicleBrand].BrandId
+
+RIGHT JOIN Suburb
+ON [Scrapyard].SuburbId = [Suburb].SuburbId
+
+GROUP BY [Scrapyard].ScrapyardOwner, QuantityAcquired, ScrapyardName, [CarPart].CarPartName, [VehicleBrand].BrandName,
+	[Suburb].SuburbName, [Suburb].City, [Vehicle].VehicleModel, [Vehicle].VehicleYear, [Scrapyard].PhoneNumber, 
+	[Scrapyard].Email, [Scrapyard].StreetAddress, [Acquisition].AcquisitionDate
 
 GO
