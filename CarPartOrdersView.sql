61c296aae91a980b967ecf8fe93ea25d18c9893e
Tsepo-G <122514307+Tsepo-G@users.noreply.github.com>
2023-02-23 12:51:31 +0200

    Update CarPartOrdersView.sql

--- CarPartOrdersView.sql
+++ CarPartOrdersView.sql
@@ -3,11 +3,30 @@ GO
 
 CREATE VIEW vPartOrders
 AS
-SELECT [CarPartOrder].OrderId, [CarPartOrder].CarPartId, [CarPart].CarPartName, [Orders].CustomerId, QuantityOrdered
+SELECT [CarPart].CarPartName, [VehicleBrand].BrandName, [Vehicle].VehicleModel, [Vehicle].VehicleYear,
+	[CarPart].RetailPrice, [Customer].FirstName, [Customer].LastName, [Customer].PhoneNumber, [Customer].Email,[Suburb].SuburbName, [Suburb].City,
+	[Customer].StreetAddress, QuantityOrdered, [Orders].OrderDate
+
 FROM CarPartOrder RIGHT JOIN Orders 
 ON [CarPartOrder].OrderId = [Orders].OrderId
+
 RIGHT JOIN CarPart
 ON [CarPartOrder].CarPartId = [CarPart].CarPartId
-GROUP BY [CarPartOrder].OrderId,[Orders].CustomerId, QuantityOrdered, [CarPartOrder].CarPartId, [CarPart].CarPartName
+
+RIGHT JOIN Customer
+ON [Orders].CustomerId = [Customer].CustomerId
+
+RIGHT JOIN Vehicle
+ON [CarPart].VehicleId = [Vehicle].VehicleId
+
+RIGHT JOIN VehicleBrand
+ON [Vehicle].VehicleId = [VehicleBrand].BrandId
+
+RIGHT JOIN Suburb
+ON [Customer].SuburbId = [Suburb].SuburbId
+
+GROUP BY QuantityOrdered, [CarPart].CarPartName, [Customer].FirstName, [Customer].LastName, [CarPart].RetailPrice,
+	[Orders].OrderDate, [Vehicle].VehicleModel, [Vehicle].VehicleYear, [VehicleBrand].BrandName, [Customer].PhoneNumber,
+	[Customer].Email, [Suburb].SuburbName, [Suburb].City, [Customer].StreetAddress
 
 GO
